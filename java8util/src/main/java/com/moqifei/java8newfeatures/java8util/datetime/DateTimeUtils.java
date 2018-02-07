3f139f0d95430e38f8db6d0027b0efaa679593da
moqifei <541831173@qq.com>
2018-02-07 17:16:34 +0800

    增加新方法及单测

--- java8util/src/main/java/com/moqifei/java8newfeatures/java8util/datetime/DateTimeUtils.java
+++ java8util/src/main/java/com/moqifei/java8newfeatures/java8util/datetime/DateTimeUtils.java
@@ -1,5 +1,6 @@
 package com.moqifei.java8newfeatures.java8util.datetime;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,22 +9,125 @@ import java.time.format.DateTimeFormatter;
  * <b>Description:Date/Time API (JSR 310):https://jcp.org/en/jsr/detail?id=310
  * </p>
  *
- * @author   moqifei
- * @version  1.0.0
- * @since    2018年02月07日
+ * @author moqifei
+ * @version 1.0.0
+ * @since 2018年02月07日
  */
 public abstract class DateTimeUtils {
-    public static final DateTimeFormatter RETURN_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public static final DateTimeFormatter YYYYMMDD = DateTimeFormatter.ofPattern("yyyyMMdd");
-    public static final DateTimeFormatter YYYYMMDDHHMMSS = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-    public static final DateTimeFormatter HHMMSS = DateTimeFormatter.ofPattern("HHmmss");
-    
-    /**
-     * 获取本地日期
-     * @param localDateTime
-     * @return
-     */
-    public static String getLocalDate(LocalDateTime localDateTime){
-    	return localDateTime.toLocalDate().toString();
-    }
+	public static final DateTimeFormatter RETURN_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	public static final DateTimeFormatter YYYYMMDD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	public static final DateTimeFormatter YYYYMMDDHHMMSS = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+	public static final DateTimeFormatter HHMMSSSSS = DateTimeFormatter.ofPattern("HHmmssSSS");
+
+	/**
+	 * 获取本地日期
+	 * 
+	 * @param localDateTime
+	 * @return
+	 */
+	public static String getLocalDate(LocalDateTime localDateTime, DateTimeFormatter dateTimeFormatter) {
+		if (dateTimeFormatter == null) {
+			dateTimeFormatter = YYYYMMDD;
+		}
+		return localDateTime.format(dateTimeFormatter);
+	}
+
+	/**
+	 * 获取本地时间
+	 * 
+	 * @param localDateTime
+	 * @return
+	 */
+	public static String getLocalTime(LocalDateTime localDateTime, DateTimeFormatter dateTimeFormatter) {
+		if (dateTimeFormatter == null) {
+			dateTimeFormatter = HHMMSSSSS;
+		}
+		return localDateTime.format(dateTimeFormatter);
+	}
+	
+	/**
+	 * 获取今天日期
+	 * @param dateFormatter
+	 * @return
+	 */
+	public static String getTodayDate(String dateFormatter){
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = null;
+		if(dateFormatter == null || "".equals(dateFormatter)){
+			dateTimeFormatter = YYYYMMDD;
+		}else{
+			dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormatter);
+		}
+		return getLocalDate(now, dateTimeFormatter);
+	}
+	
+	/**
+	 * 获取昨天日期
+	 * @param dateFormatter
+	 * @return
+	 */
+	public static String getYestertodayDate(String dateFormatter){
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = null;
+		if(dateFormatter == null || "".equals(dateFormatter)){
+			dateTimeFormatter = YYYYMMDD;
+		}else{
+			dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormatter);
+		}
+		return getLocalDate(now.minusDays(1), dateTimeFormatter);
+	}
+	
+	/**
+	 * 获取明天日期
+	 * @param dateFormatter
+	 * @return
+	 */
+	public static String getTommorrowDate(String dateFormatter){
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = null;
+		if(dateFormatter == null || "".equals(dateFormatter)){
+			dateTimeFormatter = YYYYMMDD;
+		}else{
+			dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormatter);
+		}
+		return getLocalDate(now.plusDays(1), dateTimeFormatter);
+	}
+	
+	/**
+	 * 获取当前时间
+	 * @param timeFormatter
+	 * @return
+	 */
+	public static String getNowTime(String timeFormatter){
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = null;
+		if(timeFormatter == null || "".equals(timeFormatter)){
+			dateTimeFormatter = HHMMSSSSS;
+		}else{
+			dateTimeFormatter = DateTimeFormatter.ofPattern(timeFormatter);
+		}
+		return getLocalTime(now,dateTimeFormatter);
+	}
+	
+	/**
+	 * 获取某一天星期几
+	 * @param date
+	 * @param dateformat
+	 * @return
+	 */
+	public static String getDayofWeek(String date, String dateformat){
+		if(date == null || "".equals(date)){
+			return LocalDateTime.now().getDayOfWeek().toString();
+		}
+		if(dateformat == null || "".equals(dateformat)){
+			return LocalDate.parse(date, YYYYMMDD).getDayOfWeek().toString();
+		}
+		return LocalDate.parse(date,DateTimeFormatter.ofPattern(dateformat)).getDayOfWeek().toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getDayofWeek("2018-02-06",""));
+		System.out.println(getDayofWeek("20180206","yyyyMMdd"));
+	}
+
 }
--- java8util/src/test/java/com/moqifei/java8newfeatures/java8util/datetime/DateTimeUtilsTest.java
+++ java8util/src/test/java/com/moqifei/java8newfeatures/java8util/datetime/DateTimeUtilsTest.java
@@ -2,6 +2,8 @@ package com.moqifei.java8newfeatures.java8util.datetime;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.Test;
 
@@ -9,7 +11,7 @@ import junit.framework.Assert;
 
 /**
  * <p>
- * <b>DateTimeUtils测试工具类
+ * <b>DateTimeUtils测试类
  * </p>
  *
  * @author   moqifei
@@ -20,11 +22,50 @@ public class DateTimeUtilsTest {
 	@Test
 	public void testgetLocalDate() throws Exception{
 		LocalDateTime currentTime = LocalDateTime.now();
-		
-		LocalDate date1 = currentTime.toLocalDate();
-		
-		String date = DateTimeUtils.getLocalDate(currentTime);
-		
-		Assert.assertEquals(date1.toString(), date);
+		String date = DateTimeUtils.getLocalDate(currentTime,null);
+		LocalDate date1 = LocalDate.now();
+		Assert.assertEquals(date, date1.toString());
 	}
+	
+	@Test
+	public void testgetLocalTime() throws Exception{
+		LocalDateTime currentTime = LocalDateTime.now();
+		String time = DateTimeUtils.getLocalTime(currentTime, DateTimeFormatter.ofPattern("HHmmss"));
+		LocalTime time1 = LocalTime.now();
+		
+		Assert.assertEquals(time,time1.format(DateTimeFormatter.ofPattern("HHmmss")));
+	}
+	
+	@Test
+	public void testgetTodayDate() throws Exception{
+		String today = DateTimeUtils.getTodayDate("");
+		LocalDate today1 = LocalDate.now();
+		Assert.assertEquals(today, today1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+	}
+	
+	@Test
+	public void testgetYestertodayDate() throws Exception{
+		String yestertoday = DateTimeUtils.getYestertodayDate("");
+		LocalDate yestertoday1 = LocalDate.now();
+		Assert.assertEquals(yestertoday, yestertoday1.minusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+	}
+	
+	@Test
+	public void testgetTommorrowDate() throws Exception{
+		String tommorrow = DateTimeUtils.getTommorrowDate("");
+		LocalDate tommorrow1 = LocalDate.now();
+		Assert.assertEquals(tommorrow, tommorrow1.plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+	}
+	
+	@Test
+	public void testgetDayofWeek() throws Exception{
+		String dayofWeek = LocalDate.now().getDayOfWeek().toString();
+		Assert.assertEquals(dayofWeek, DateTimeUtils.getDayofWeek("", ""));
+		String dayofWeek1 = LocalDate.parse("2018-02-07").getDayOfWeek().toString();
+		Assert.assertEquals(dayofWeek1, DateTimeUtils.getDayofWeek("2018-02-07", ""));
+		String dayofWeek2 = LocalDate.parse("20180207", DateTimeFormatter.ofPattern("yyyyMMdd")).getDayOfWeek().toString();
+		Assert.assertEquals(dayofWeek2, DateTimeUtils.getDayofWeek("20180207", "yyyyMMdd"));
+		
+	}
+	
 }
